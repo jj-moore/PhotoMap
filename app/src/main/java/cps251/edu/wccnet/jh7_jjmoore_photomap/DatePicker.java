c75062491a9e20c3540c1ddfb696107fe3e57bb8
Jeremy Moore <jeremy00moore@gmail.com>
2017-05-06 00:12:27 -0400

    Added icon Pull Date from Image ExifInterface Added Date and DatePicker Interface

---
+++ app/src/main/java/cps251/edu/wccnet/jh7_jjmoore_photomap/DatePicker.java
@@ -0,0 +1,32 @@
+package cps251.edu.wccnet.jh7_jjmoore_photomap;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.os.Bundle;
+
+import java.util.Calendar;
+
+public class DatePicker extends DialogFragment implements DatePickerDialog.OnDateSetListener {
+    private DatabaseManager dbManager;
+    private int id;
+
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        super.onCreateDialog(savedInstanceState);
+        dbManager = (DatabaseManager) getArguments().getSerializable("dbManager");
+        id = getArguments().getInt("id");
+
+        final Calendar calendar = Calendar.getInstance();
+        final int year = getArguments().getInt("year", calendar.get(Calendar.YEAR));
+        final int month = getArguments().getInt("month", calendar.get(Calendar.MONTH));
+        final int day = getArguments().getInt("day", calendar.get(Calendar.DAY_OF_MONTH));
+        return new DatePickerDialog(getActivity(), this, year, month, day);
+    }
+
+    public void onDateSet(android.widget.DatePicker view, int year, int month, int date) {
+        final Calendar calendar = Calendar.getInstance();
+        calendar.set(year, month, date);
+        dbManager.update(id, calendar.getTimeInMillis());
+    }
+
+}
